182dfa0cf5096705d21dc02de8c08fb243a794b9
Barnabas Asha <barnabee58@gmail.com>
2023-07-22 02:00:12 +0100

    Add dataabase logging

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,13 @@ server.listen(3000, function () {
   console.log("Server running on port 3000");
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach((client) => {
+    client.close();
+  });
+  server.close(() => shutdownDB());
+});
+
 // Begin Websockets
 const WebSocketServer = require("ws").Server;
 
@@ -27,6 +34,11 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server");
   }
 
+  db.run(`
+        INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+  `);
+
   ws.on("close", function close() {
     wss.broadcast(`Current visitors: ${numClients}`);
     console.log("A client has disconnected");
@@ -38,3 +50,30 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+// end websockets
+// begin database
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+
+        )
+    `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Shutting down db");
+  db.close();
+}
